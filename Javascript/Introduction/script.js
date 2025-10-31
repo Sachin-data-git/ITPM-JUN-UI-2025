95c21757794e88ecb8360866576b1d755b4c57ee
sachin <sachin.g@itpreneurpune.com>
2025-10-31 13:00:48 +0530

    javascript added

---
+++ Javascript/Introduction/script.js
@@ -0,0 +1,65 @@
+// alert("This is External");
+// confirm("Mail sent successfully....");
+
+
+// var name=prompt("Enter Your Name");
+// document.write(name);
+// console.log(name);
+
+
+// var demo=null;
+// console.log(demo)
+
+
+
+// var , let ,const
+
+// var - outdated
+
+
+// we cant redeclare the variables as well as we can re-assign the values
+// var a=10;
+// var a=20;
+// console.log(a);
+
+// var name="aman";
+// var name="atul";
+
+
+
+// let - updated
+// we can't redeclare the variables but we can re-assign the values
+
+// let b=20;
+//     b=30;
+// console.log(b);
+
+// const - updated
+// we can't redeclare the variablesas well as  can't re-assign the values
+const bdate="20/10/2000";
+      bdate="30/10/2000";
+
+      console.log(bdate);
+
+
+    //   demo="arav";
+
+
+
+// TASK
+// 1. print your full name using let var and const keywords (input from user)
+//2. show the diffrence between let and var 
+//3. show the diffrence between let and const  
+//4. show the diffrence between let ,var  and const  
+//5.  print your introduction  in console
+//6. print a paragraph using document.write()
+//7.  use prompt alert and confirm
+
+
+
+
+
+
+
+
+
