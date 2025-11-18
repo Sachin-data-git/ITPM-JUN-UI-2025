d9a2e19c3422d9eafbbc379c8ec56a98f27afca2
sachin <sachin.g@itpreneurpune.com>
2025-11-18 13:05:57 +0530

    Window and Keyboard event task are added in index file kindly check and solve the questions

---
+++ Javascript/ImportExport/script1.js
@@ -0,0 +1,5 @@
+let fname="Ashish";
+let lname="Dange";
+
+// export default fname;
+export  {fname,lname};
---
+++ Javascript/ImportExport/script2.js
@@ -0,0 +1,3 @@
+import { fname as firstName,lname} from './script1.js'
+
+console.log(firstName  + " "+ lname);
